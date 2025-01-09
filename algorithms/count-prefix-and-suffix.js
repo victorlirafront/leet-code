205a06de986a490acb8aa8bb94754519dc90b31a
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-01-08 22:25:35 -0300

    feat: add algorithms

--- algorithms/count-prefix-and-suffix.js
+++ algorithms/count-prefix-and-suffix.js
@@ -1,13 +1,10 @@
 // const words1 = ["a", "aba", "ababa", "aa"];
-
 //eu preciso a primeira string com as seguintes e verificar se o prefixo e sulfixo são iguais 
 // e a segunda string (str2) deve ser maior ou igual à primeira string
 
-
-
 // Função que verifica se str1 é prefixo e sufixo de str2
 function isPrefixAndSuffix(str1, str2) {
-  return str2.startsWith(str1) && str2.endsWith(str1);
+  return str2.startsWith(str1) && str2.endsWith(str1) && str2.length >= str1.length;
 }
 
 // Função para contar os pares válidos
@@ -23,22 +20,24 @@ function countPrefixSuffixPairs(words) {
       }
     }
   }
-  
+
   return count;
 }
 
-
 // Exemplo 1
-const words1 = ["a", "aba", "ababa", "aa"];
-console.log(countPrefixSuffixPairs(words1));  // Saída: 4
+const words1 = ['a', 'aba', 'ababa', 'aa'];
+console.log(countPrefixSuffixPairs(words1)); // Saída: 4
 
 // Exemplo 2
-const words2 = ["pa", "papa", "ma", "mama"];
-console.log(countPrefixSuffixPairs(words2));  // Saída: 2
+const words2 = ['pa', 'papa', 'ma', 'mama'];
+console.log(countPrefixSuffixPairs(words2)); // Saída: 2
 
 // Exemplo 3
-const words3 = ["abab", "ab"];
-console.log(countPrefixSuffixPairs(words3));  // Saída: 0
+const words3 = ['abab', 'ab'];
+console.log(countPrefixSuffixPairs(words3)); // Saída: 0
+
+
+
 
 // Caso 2: words2 = ["pa", "papa", "ma", "mama"]
 // Pares válidos:
