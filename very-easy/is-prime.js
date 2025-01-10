bfdc554d8bfa6051245e37842a5dfe49be58a0c0
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-01-10 16:14:15 -0300

    feat: is prime

---
+++ very-easy/is-prime.js
@@ -0,0 +1,16 @@
+// Math.sqrt é uma função em JavaScript que calcula a raiz quadrada de um número.
+
+function isPrime(number) {
+  if (number <= 1) return false; // Números <= 1 não são primos
+
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false; // Encontrou um divisor, não é primo
+    }
+  }
+
+  return true; // Nenhum divisor encontrado, é primo
+}
+
+const result = isPrime(10);
+console.log(result); // false
